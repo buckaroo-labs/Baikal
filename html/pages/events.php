8af3ae39d8386f0f889692ea1870c3485823c74d
buckaroo-labs <heiner@monstro.us>
2026-03-01 07:26:45 -0800

    add calendar list to events page

--- html/pages/events.php
+++ html/pages/events.php
@@ -3,12 +3,17 @@
 //https://sabre.io/vobject/vcard/
 use Sabre\VObject;
 if (isset($_SESSION['username'])) {
+    $and='';
+    if (isset($_GET['calendarid']) && is_numeric($_GET['calendarid'])) {
+        $calendarid=$_GET['calendarid'];
+        $and=" AND i.id=" . $calendarid;
+    }
     $columns=" c.id, c.uri, c.calendardata, i.principaluri as owner, i.displayname as calendarname ";
     $from=" FROM calendarobjects c INNER JOIN calendarinstances i on c.calendarid=i.id ";
     $where=" WHERE i.principaluri='principals/" . $_SESSION['username'] . "'";
-    $sql="SELECT count(*) " . $from . $where;
+    $sql="SELECT count(*) " . $from . $where . $and;
     $resultcount=$dds->getInt($sql);
-    $sql="SELECT " . $columns . $from . $where;
+    $sql="SELECT " . $columns . $from . $where . $and;
     $dds->setMaxRecs(9999);
     $result=$dds->setSQL($sql);
     echo '<div id="Events" class="w3-twothird w3-container" style="overflow:hidden">';
@@ -27,13 +32,25 @@ if (isset($_SESSION['username'])) {
             $starttime= $dtstart->format(\DateTime::W3C);
             $dtend = $vevent->DTEND->getDateTime();
             $endtime= $dtend->format(\DateTime::W3C);
-            echo ('<tr><td><a href="index.php?p=event&id='.$rrow['id'].'">'.$rrow['id'].'</a></td><td>'.$summary.'</td><td>'.$starttime.'</td><td>'.$endtime.'</td></tr>'); 
+            echo ('<tr><td><a href="index.php?p=event&id='.$rrow['id'].'">'.$rrow['id'].'</a></td><td>'.$summary.'</td><td>'.$starttime.'</td><td>'.$rrow['calendarname'].'</td></tr>'); 
              //echo ('<tr><td>'.$rrow['id'].'</td><td><span class="vcarddata">'.$rrow['calendardata'].'</span></td><td>'.$summary.'</td><td>'.$starttime.'</td><td>'.$rrow['calendarname'].'</td></tr>'); 
         } 
 
     //start, end, summary
     }
     echo "</table>\n</div>";
+
+    echo '<div id="calendarlist"><h4>Calendars</h4><ul>';
+    $sql="SELECT id, displayname FROM calendarinstances WHERE principaluri='principals/" . $_SESSION['username'] . "' order by FIELD(displayname," .'"Default Calendar"' .") DESC, displayname";
+    $result=$dds->setSQL($sql);
+    while ($rrow=$dds->getNextRow('assoc')) {
+        $liClassAndID=' class="calendarname"';
+        if(isset($calendarid) && $calendarid==$rrow['id'])  {
+            $liClassAndID .= ' id="activeCalendarName" ';
+        }
+        echo '<li '. $liClassAndID .'"><a href="index.php?p=events&calendarid=' . $rrow['id'] . '">' . $rrow['displayname'] . '</a></li>';
+    }
+    echo '</ul></div>';
 } else {
     //must log in
     echo "You must be logged in to use this page.";
