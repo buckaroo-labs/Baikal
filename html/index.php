c8a98d8ad4dbb8289ef2d7653dda8a71133f772b
buckaroo-labs <heiner@monstro.us>
2026-03-08 12:17:14 -0700

    help and licensing information

--- html/index.php
+++ html/index.php
@@ -57,7 +57,13 @@ require_once("Hydrogen/db/clsDataSource.php");
 			} else {
 				echo "<p>This is " . $settings['appname']. ', a fork of <a href="https://sabre.io/baikal/" target="_blank">Ba&iuml;kal</a>, which is a calendar and contacts server built on <a href="https://sabre.io/dav/" target="_blank">sabre/dav</a>.</p>';
 
-				echo "<p>Use the Admin link for Ba&iuml;kal&apos;s management features. Use the Explorer link for sabre&apos;s folder navigation interface. Use the Contacts, Events, Reminders and Journal links for viewing your data.</p>";
+				echo "<p>Use the Admin link for Ba&iuml;kal&apos;s management features. Use the Explorer link for sabre&apos;s folder navigation interface. Use the <dfn>Contacts</dfn>, <dfn>Events</dfn>, <dfn>Tasks</dfn>, <dfn>Journal</dfn> and <dfn>Alarms</dfn> links for viewing your data.</p>";
+
+				echo "<p>Additional applications, also based on the VCALENDAR standard, include:</p><ul>
+				<li><dfn>Reminders</dfn> for enhanced management of recurring tasks. These items are stored as VTODO components in a separate calendar and their recurrence is managed on the server via the web UI. Changes made by external client software to recurrence rules are ignored.</li>
+				<li><dfn>Lists</dfn> for managing grouped tasks like shopping lists or packing lists. These are also stored as VTODO components in a separate calendar.</li>
+				<li><dfn>Time</dfn> for recording time spent on projects. Entries are stored as VJOURNAL components in a separate calendar.</li>
+				</ul>";
 				echo '</div>';
 			}
         }      
