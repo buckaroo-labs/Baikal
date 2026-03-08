d34847ea6f109d9990c47cfbfe4d16726249f078
buckaroo-labs <heiner@monstro.us>
2026-03-08 13:55:59 -0700

    reduce clutter of encoded photo data

--- html/pages/contact.php
+++ html/pages/contact.php
@@ -25,32 +25,20 @@ function saveimage($data, $photofullpath) {
     return true; 
 }
 
-function base64_to_jpeg($base64_string, $photofullpath) {
-    // open the output file for writing
-    if (!$ifp = fopen($photofullpath,'wb')) {
-        debug( "Cannot open file ($photofullpath)");
-        //exit;
-        return false;
-    } 
-    // split the string on commas
-    // $data[ 0 ] == "data:image/jpeg;base64"
-    // $data[ 1 ] == <actual base64 string>
-    $data = explode( ',', $base64_string );
-
-    if (fwrite($ifp,  base64_decode( $data[ count($data)-1 ] ) ) === FALSE) {
-                debug( "Cannot write to file ($photofullpath)");
-                //exit;
-                return false;
-    } else {
-                debug( "Photo data written to file ($photofullpath)");
-         
-    }
-    // clean up the file resource
-    fclose( $ifp ); 
-
-    return Strue; 
-}
-
+/*
+supported properties:
+FN
+N
+ORG
+CATEGORIES
+TITLE
+ADR
+EMAIL
+PHONE
+PHOTO
+NOTE
+?attachments
+*/
 if (isset($_SESSION['username'])) {
     if (isset($_GET['id']) && is_numeric($_GET['id'])) {
         $contactid=$_GET['id'];
@@ -60,19 +48,18 @@ if (isset($_SESSION['username'])) {
     $columns=" c.id, c.uri, c.carddata, a.principaluri as owner, a.displayname as book_name ";
     $from=" FROM cards c INNER JOIN addressbooks a on c.addressbookid=a.id ";
     $where=" WHERE a.principaluri='principals/" . $_SESSION['username'] . "' and c.id=" . $contactid;
-    $sql="SELECT count(*) " . $from . $where;
-    $resultcount=$dds->getInt($sql);
-    $dds->setMaxRecs(9999);
     $sql="SELECT " . $columns . $from . $where;
     $result=$dds->setSQL($sql);
 
     error_reporting(E_ERROR | E_PARSE);
     while ($rrow=$dds->getNextRow('assoc')) {
         $vcard = VObject\Reader::read($rrow['carddata']);
- 
-    echo '<div id="Contacts" class="w3-twothird w3-container" style="overflow:hidden">';
+        $vcardsm =VObject\Reader::read($rrow['carddata']);
+        unset($vcardsm->PHOTO);
 
-        echo ('<table id="vcardtable" class="table sortable" style="clear:both">' . "\n" . '<tr><th>ID</th><th>Categories</th><th>Display Name</th><th>Book</th></tr>' . "\n");
+        echo '<div id="Contact" class="w3-twothird w3-container" style="overflow:hidden">';
+        echo '<h2 id="ContactName">' . $vcard->FN . '</h2>';
+        echo ('<table id="vcardtable" class="table sortable" style="clear:both">' . "\n" . '<tr><th>ID</th><th>Categories</th><th>Organization</th><th>Book</th></tr>' . "\n");
         $telephone='';
         foreach($vcard->TEL as $tel) {
             $telephone.="Phone";
@@ -97,26 +84,19 @@ if (isset($_SESSION['username'])) {
             }
             $addresses .= ": " . str_replace(";","|",$adr) . "<BR>\n";
         }
-        echo ('<tr><td>'.$rrow['id'].'</td><td>'.$vcard->CATEGORIES.'</td><td class="bold">'.$vcard->FN.'</td><td>'.$rrow['book_name'].'</td></tr>' . "\n");
+        echo ('<tr><td>'.$rrow['id'].'</td><td>'.$vcard->CATEGORIES.'</td><td class="bold">'.(string) $vcard->ORG.'</td><td>'.$rrow['book_name'].'</td></tr>' . "\n");
 
         echo "</table>\n";
-        $data=str_replace("\n","<br>\n",$rrow['carddata']);
-        echo '<p><span class="vcarddata">'.$data.'</span></p></div>';
-               $photopath='';
+        $data=$vcardsm->serialize();
+        $data=str_replace("\n","<br>\n",$data);
+        echo '<p><span class="vcarddata">'.$data .'</span></p></div>';
+        
+        $photopath='';
         if (isset($vcard->PHOTO) && isset($vcard->UID)) {
-            //$photo=base64_decode($vcard->PHOTO);
-            //https://www.php.net/manual/en/function.base64-decode.php
-            //$decoded = "";
-            //for ($i=0; $i < ceil(strlen($vcard->PHOTO)/256); $i++)
-            //$decoded = $decoded . base64_decode(substr($vcard->PHOTO,$i*256,256));
-
-            //OMG was it really this easy all the time?
             $decoded=$vcard->PHOTO;
             $photopath='contactphotos/' . $vcard->UID . '.' . strtolower($vcard->PHOTO['TYPE']);
             $photofullpath= __DIR__ . '/../' . $photopath;
             if(saveimage($decoded,$photofullpath)) echo '<img src="' . $photopath . '" class="contactphoto">';
-            //https://stackoverflow.com/questions/15153776/convert-base64-string-to-an-image-file
-            //if(base64_to_jpeg($decoded,$photofullpath)) echo '<img src="' . $photopath . '" style="max-width:400px;">';
 
         }
     }
