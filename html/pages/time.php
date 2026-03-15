ae95a51ee59b07a7d2819fd488a046d1033d8fb2
buckaroo-labs <heiner@monstro.us>
2026-03-15 09:15:39 -0700

    time entry

--- html/pages/time.php
+++ html/pages/time.php
@@ -10,12 +10,13 @@ $tdata_xform[1]=array('','');
 $tdata_xform[2]=array('datetimeformat','');
 
 $where=" WHERE i.uri ='projecttime'";
-$thead=array("ID", "Summary", "Start", "End");
+$thead=array("ID", "Summary", "Start", "End", "Project");
 
 $tdata[0]=array('Q','id');
 $tdata[1]=array('V','SUMMARY');
 $tdata[2]=array('V','DTSTART');
 $tdata[3]=array('V','DTEND');
+$tdata[4]=array('V','CATEGORIES');
 
 
 include("pages/folder.php");
