c3b253e1b755f76fc95bd4a42a51c8edf66b702c
buckaroo-labs <heiner@monstro.us>
2026-04-11 16:20:16 -0700

    sql

--- html/pages/account.php
+++ html/pages/account.php
@@ -3,12 +3,11 @@
 if (isset($_SESSION['username'])) {
 
 
-    $columns=" c.id, c.components, i.displayname as calendarname, i.uri as calendaruri, i.calendarcolor ";
-    $group='c.id, c.components, i.displayname , i.uri , i.calendarcolor ';
+    $columns=" c.id, c.components, i.displayname, i.uri, i.calendarcolor ";
     $from=" FROM calendars c INNER JOIN calendarinstances i on c.id=i.calendarid ";
     $from .= " LEFT JOIN calendarobjects o on i.id=o.calendarid ";
     $where=" WHERE i.principaluri='principals/" . $_SESSION['username'] . "'";
-    $sql="SELECT count(*) as itemcount," . $columns . $from . $where . " GROUP BY " . $group;
+    $sql="SELECT count(*) as itemcount," . $columns . $from . $where . " GROUP BY " . $columns;
     $result=$dds->setSQL($sql);
 
     echo '<h3>Calendars</h3>' . '
